2693e14adb3ba8305560776e71d7085fcad437f6
bikramrai072 <bikramrai073@gmail.com>
2017-05-25 20:07:20 -0700

    Initial commit

---
+++ app/src/main/java/com/example/bikram/login/SocialMediaProvider.java
@@ -0,0 +1,19 @@
+package com.example.bikram.login;
+
+/**
+ * Created by bikram on 2/6/2017.
+ */
+
+    public enum SocialMediaProvider {
+
+        FACEBOOK,
+        TWITTER,
+        GOOGLE_PLUS,
+        INSTAGRAM,
+        LINKED_IN,
+        YAHOO,
+        WINDOWS_LIVE,
+        GITHUB
+
+    }
+
